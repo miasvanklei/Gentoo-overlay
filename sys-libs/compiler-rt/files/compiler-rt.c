0a365b7d56aaa0639c7332b564b315f3a62251f4
Mias van Klei <miasvanklei@outlook.com>
2018-02-23 17:32:41 +0100

    add extra function needed for julia

--- sys-libs/compiler-rt/files/compiler-rt.c
+++ sys-libs/compiler-rt/files/compiler-rt.c
@@ -263,3 +263,14 @@ tu_int __umodti3(tu_int a, tu_int b)
     __udivmodti4(a, b, &r);
     return r;
 }
+
+ti_int __divti3(ti_int a, ti_int b)
+{
+    const int bits_in_tword_m1 = (int)(sizeof(ti_int) * CHAR_BIT) - 1;
+    ti_int s_a = a >> bits_in_tword_m1;           /* s_a = a < 0 ? -1 : 0 */
+    ti_int s_b = b >> bits_in_tword_m1;           /* s_b = b < 0 ? -1 : 0 */
+    a = (a ^ s_a) - s_a;                         /* negate if s_a == -1 */
+    b = (b ^ s_b) - s_b;                         /* negate if s_b == -1 */
+    s_a ^= s_b;                                  /* sign of quotient */
+    return (__udivmodti4(a, b, (tu_int*)0) ^ s_a) - s_a;  /* negate if s_a == -1 */
+}
