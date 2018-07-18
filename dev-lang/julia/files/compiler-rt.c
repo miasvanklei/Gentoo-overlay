695610943dc94ac3f62ade3e1772c5c0d2d9b370
Mias van Klei <miasvanklei@outlook.com>
2018-07-18 15:41:15 +0200

    add missing function

--- dev-lang/julia/files/compiler-rt.c
+++ dev-lang/julia/files/compiler-rt.c
@@ -254,3 +254,16 @@ ti_int __divti3(ti_int a, ti_int b)
     s_a ^= s_b;                                  /* sign of quotient */
     return (__udivmodti4(a, b, (tu_int*)0) ^ s_a) - s_a;  /* negate if s_a == -1 */
 }
+
+ti_int
+__modti3(ti_int a, ti_int b)
+{
+    const int bits_in_tword_m1 = (int)(sizeof(ti_int) * CHAR_BIT) - 1;
+    ti_int s = b >> bits_in_tword_m1;  /* s = b < 0 ? -1 : 0 */
+    b = (b ^ s) - s;                   /* negate if s == -1 */
+    s = a >> bits_in_tword_m1;         /* s = a < 0 ? -1 : 0 */
+    a = (a ^ s) - s;                   /* negate if s == -1 */
+    tu_int r;
+    __udivmodti4(a, b, &r);
+    return ((ti_int)r ^ s) - s;                /* negate if s == -1 */
+}
