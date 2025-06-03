c91a452aa3c8c503166ff3cadfb277eb559be08d
Mias van Klei <miasvanklei@outlook.com>
2025-06-03 20:43:02 +0200

    sys-libs/widevine-compat: add missing symbols for arm64

--- sys-libs/widevine-compat/files/widevine-compat.c
+++ sys-libs/widevine-compat/files/widevine-compat.c
@@ -4,6 +4,9 @@
 #include <wchar.h>
 #include <malloc.h>
 #include <pthread.h>
+#include <setjmp.h>
+#include <fcntl.h>
+#include <stdarg.h>
 
 int __register_atfork(void (*prepare) (void), void (*parent) (void), void (*child) (void), void * __dso_handle)
 {
@@ -43,6 +46,47 @@ char * __strncat_chk(char * s1, const char * s2, size_t n, size_t s1len)
 char * __strdup(const char * s)
 {
     return strdup(s);
+}
+
+int __open64_2(const char *path, int oflag)
+{
+    return open(path, oflag);
+}
+
+int __vfprintf_chk(FILE *stream, int flag, const char *format, va_list ap)
+{
+    return vfprintf(stream, format, ap);
+}
+
+int __vasprintf_chk(char **strp, int flag, const char *format, va_list ap)
+{
+    return vasprintf(strp, format, ap);
+}
+
+char *__fgets_chk(char *s, size_t slen, int n, FILE *stream)
+{
+    return fgets(s, n, stream);
+}
+
+void __longjmp_chk(jmp_buf env, int val)
+{
+    return longjmp(env, val);
+}
+
+void *__memset_chk(void *s, int c, size_t n, size_t buflen)
+{
+	return memset(s, c, n);
+}
+
+int fcntl64(int fd, int cmd, ...) {
+	int ret;
+	va_list va;
+
+	va_start(va, cmd);
+	ret = fcntl(fd, cmd, va);
+	va_end(va);
+
+	return ret;
 }
 
 void *__libc_stack_end;
